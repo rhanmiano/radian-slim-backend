8352a7df8e19f6851ac39c30db009ec5537f8ed2
Rhan Miano <rhanmiano29@gmail.com>
2021-07-06 13:05:49 +0800

    chore: update handling of environment variables in public/index.php

--- public/index.php
+++ public/index.php
@@ -1,10 +1,17 @@
 <?php
 
 $rootPath = dirname(dirname(__FILE__));
+// var_dump($rootPath); die();
 require $rootPath  . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::create($rootPath);
-$dotenv->load();
+if (file_exists($rootPath . '/.env')) {
+  $dotenv = Dotenv\Dotenv::create($rootPath);
+  $dotenv->load();
+}
+
+if (getenv('ENVIRONMENT') === false) {
+  echo 'Application\'s environment configuration has not been set up properly.'; die();
+}
 
 $app = (new App\Init())->getApp();
 
